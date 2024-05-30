9cf3bb64cc8dd136c7c4836b95cc7f09905ed1d6
bivhunter <bivhunter@ukr.net>
2024-05-30 15:32:10 +0300

    configured env

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,33 +2,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
+
 export interface SignInRequest {
-  password: string;
-  username: string;
+    password: string;
+    username: string;
 }
 
-// TODO moved to env
-const baseUrl: string = 'https://6zuyha9k59.execute-api.us-east-1.amazonaws.com/prod';
+// TODO move to interceptor
+const baseUrl: string = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    private http: HttpClient,
-  ) {}
+    constructor(private http: HttpClient) {}
 
-  public signIn(signInData: SignInRequest): Observable<string> {
-    // TODO move to interceptor
-    const headers = new HttpHeaders({
-      'x-api-key': 'JlR2Q5vf8k2XIPkk4pDAN0RtxTASVZ6lxlz7Vr70',
-    })
-    return this.http.post<{accessToken: string}>(`${baseUrl}/auth/signin`, signInData, {headers}).pipe(
-      map((response: {accessToken: string}) => response.accessToken),
-      catchError((e: Error) => {
-        console.log(e)
-        return 'error'
-      })
-    );
-  } 
+    public signIn(signInData: SignInRequest): Observable<string> {
+        // TODO move to interceptor
+        const headers = new HttpHeaders({
+            'x-api-key': 'JlR2Q5vf8k2XIPkk4pDAN0RtxTASVZ6lxlz7Vr70',
+        });
+        return this.http
+            .post<{ accessToken: string }>(
+                `${baseUrl}/auth/signin`,
+                signInData,
+                { headers }
+            )
+            .pipe(
+                map(
+                    (response: { accessToken: string }) => response.accessToken
+                ),
+                catchError((e: Error) => {
+                    console.log(e);
+                    return 'error';
+                })
+            );
+    }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment: {production: boolean; baseUrl: string} = {
+  production: true,
+  baseUrl: 'https://6zuyha9k59.execute-api.us-east-1.amazonaws.com/prod',
+};
